074e49f1de518bcaa5bc5ef11880c500ac0f2b0e
Mac-Anthony <142856088+tony-dev0@users.noreply.github.com>
2025-08-02 23:22:34 +0100

    minor changes

--- client/src/page/home.tsx
+++ client/src/page/home.tsx
@@ -1,40 +1,38 @@
-import { useState, useEffect } from 'react'
-import Head from '../sections/Head'
+import { useState, useEffect } from "react";
+import Head from "../sections/Head";
 import {
   Featured,
   Topitems,
   Limitedstocks,
   Maingadget,
-} from '../sections/Products'
-import { Desc, Advert, Reviews } from '../sections/About'
-import { Contact } from '../sections/Contact'
-import { Footer } from '../sections/Footer'
-import '../styles/font-awesome.min.css'
-import '../styles/bootstrap/dist/css/bootstrap.min.css'
-import '../styles/style.css'
-import '../styles/responsive.css'
-import { useDispatch } from 'react-redux'
-import { storeProducts } from '../redux/product/productSlice'
-import axios from 'axios'
+} from "../sections/Products";
+import { Desc, Advert, Reviews } from "../sections/About";
+import { Contact } from "../sections/Contact";
+import { Footer } from "../sections/Footer";
+import "../styles/font-awesome.min.css";
+import "../styles/bootstrap/dist/css/bootstrap.min.css";
+import "../styles/style.css";
+import "../styles/responsive.css";
+import { useDispatch } from "react-redux";
+import { storeProducts } from "../redux/product/productSlice";
+import axios from "axios";
 
 export const Home = () => {
-  const dispatch = useDispatch()
-  const [loadscreen, setloadScreen] = useState(false)
+  const dispatch = useDispatch();
+  const [loadscreen, setloadScreen] = useState(false);
   useEffect(() => {
-    setloadScreen(true)
-    setTimeout(() => {
-      setloadScreen(false)
-    }, 2000)
-  }, [])
-
-  axios
-    .get('/api/products')
-    .then((res) => {
-      dispatch(storeProducts(res.data))
-    })
-    .catch((error) => {
-      console.log(`Backend Error: ${error}`)
-    })
+    setloadScreen(true);
+    axios
+      .get("/api/products")
+      .then((res) => {
+        dispatch(storeProducts(res.data));
+        setloadScreen(false);
+      })
+      .catch((error) => {
+        console.log(`Backend Error: ${error}`);
+        setloadScreen(false); // Hide loader even if error
+      });
+  }, []);
 
   return (
     <>
@@ -57,5 +55,5 @@ export const Home = () => {
         </div>
       )}
     </>
-  )
-}
+  );
+};
